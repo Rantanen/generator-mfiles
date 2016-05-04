a4674f9a28aa12df307b2379323fbd073ee6f943
Mikko Rantanen <jubjub@jubjubnest.net>
2016-05-04 12:01:01 +0300

    Made eslintrc easier to configure
    Added some of the options directly in the .eslintrc.js. This makes it easier for the users to change these settings as they don't need to dig through the documentation and/or check the inherited shared config.

--- generators/app/templates/eslintrc-generic.js
+++ generators/app/templates/eslintrc-generic.js
@@ -5,5 +5,36 @@ module.exports = {
     env: {
         browser: true,
         jquery: true
+    },
+    
+    rules: {
+        
+        // "new-cap" rule enforces new-operator when calling Capitalized functions.
+        // Uncomment the following line when using M-Files COM interfaces from JS.
+        // These interfaces use Capital functions for normal functions.
+        // "new-cap": 0,
+        
+        // "no-mixed-html" rule checks for various XSS issues.
+        // The rule is specified here so it's easy to change the parameter naming rules,
+        // etc. without having to change the shared M-Files config.
+        "xss/no-mixed-html": [ "error", {
+            "htmlVariableRules": [ "AsHtml$", "HtmlEncoded$", "^html(A-Z|$)", "innerHTML" ],
+            "htmlFunctionRules": [ "\.asHtml", "HtmlEncoded$", "Html$" ],
+            "functions": {
+
+                // jQuery
+                "$": { "htmlInput": true, "safe": [ "document", "this" ] },
+                ".html": { "htmlInput": true, "htmlOutput": true },
+
+                // Passthrough
+                ".join": { "passthrough": { "obj": true, "args": true } },
+
+                // Encoding functions
+                "he.encode": { "htmlOutput": true },
+                "utilities.encodehtml": { "htmlOutput": true },
+                "utilities.encodecss": { "htmlOutput": true },
+                "Number": { "htmlOutput": true }
+            }
+        } ]
     }
 }
